f8e186de9249c4f5a5791c9b683ef45e5ecf9fb7
s4hibul <113578853+s4hibul@users.noreply.github.com>
2023-10-13 17:59:15 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,16 @@
+docker build -t muhammadsahibulsaid/from from
+
+docker build -t muhammadsahibulsaid/run run
+
+docker build -t muhammadsahibulsaid/command command
+docker container create --name command muhammadsahibulsaid/command
+
+docker build -t muhammadsahibulsaid/label label
+
+docker build -t muhammadsahibulsaid/add add
+
+docker build -t muhammadsahibulsaid/ignore ignore
+
+docker build -t muhammadsahibulsaid/expose expose
+
+docker build -t muhammadsahibulsaid/env env
